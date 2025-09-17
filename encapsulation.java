1dbe4cc6d6478b1bd31e6bb2cadd72322efbf5e1
Prerna <prernapalakbnk@gmail.com>
2025-09-17 09:23:35 +0530

    Create encapsulation.java

---
+++ encapsulation.java
@@ -0,0 +1,35 @@
+class prerna{
+	protected int salary;
+	private int age = 19;
+	public String name = "Prerna";
+	public int getter() {
+		return age;
+	}
+	
+	public void setter(int salary) {
+		this.salary = salary;
+	}
+	public void greet(){
+		System.out.println("my name is:" +name);
+	}
+	 public int getsalary() {
+		 return salary;
+	 }
+	
+	public void addage(int a) {
+		System.out.println(19+a);
+	}
+}
+
+
+
+public class encapsulation {
+	public static void main(String[] arg) {
+		prerna g  = new prerna();
+		
+		g.greet();
+		g.setter(123654898);
+		System.out.println(g.getsalary());
+		g.addage(3);
+	}	
+}
